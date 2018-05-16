dd970110907db306f1003af8ada9d71b235363a9
Sebastian Schmidt <s.schmidt@physik.uni-bonn.de>
2018-05-16 23:34:44 +0200

    fix outliers of 80 column rule

--- src/plotly/api.nim
+++ src/plotly/api.nim
@@ -87,41 +87,53 @@ type
     yaxis*: Axis
     yaxis2*: Axis
 
-func newErrorBar*[T: SomeNumber](err: T, color: Color, thickness = 0.0, width = 0.0, visible = true, percent = false): ErrorBar[T] =
+func newErrorBar*[T: SomeNumber](err: T, color: Color, thickness = 0.0, width = 0.0,
+                                 visible = true, percent = false): ErrorBar[T] =
   ## creates an `ErrorBar` object of type `ebkConstantSym` or `ebkPercentSym`, if the `percent` flag
   ## is set to `true`
   # NOTE: there is a lot of visual noise in the creation here... change how?
   if percent == false:
-    result = ErrorBar[T](visible: visible, color: color, thickness: thickness, width: width, kind: ebkConstantSym)
+    result = ErrorBar[T](visible: visible, color: color, thickness: thickness,
+                         width: width, kind: ebkConstantSym)
     result.value = err
   else:
-    result = ErrorBar[T](visible: visible, color: color, thickness: thickness, width: width, kind: ebkPercentSym)
+    result = ErrorBar[T](visible: visible, color: color, thickness: thickness,
+                         width: width, kind: ebkPercentSym)
     result.percent = err
 
-func newErrorBar*[T: SomeNumber](err: tuple[p, m: T], color: Color, thickness = 0.0, width = 0.0, visible = true, percent = false): ErrorBar[T] =
-  ## creates an `ErrorBar` object of type `ebkConstantAsym`, constant plus and minus errors given as tuple
-  ## or `ebkPercentAsym` of `percent` flag is set to true
+func newErrorBar*[T: SomeNumber](err: tuple[p, m: T], color: Color, thickness = 0.0,
+                                 width = 0.0, visible = true, percent = false): ErrorBar[T] =
+  ## creates an `ErrorBar` object of type `ebkConstantAsym`, constant plus and
+  ## minus errors given as tuple or `ebkPercentAsym` of `percent` flag is set to true
   if percent == false:
-    result = ErrorBar[T](visible: visible, color: color, thickness: thickness, width: width, kind: ebkConstantAsym)
+    result = ErrorBar[T](visible: visible, color: color, thickness: thickness,
+                         width: width, kind: ebkConstantAsym)
     result.valuePlus  = err.p
     result.valueMinus = err.m
   else:
-    result = ErrorBar[T](visible: visible, color: color, thickness: thickness, width: width, kind: ebkPercentAsym)
+    result = ErrorBar[T](visible: visible, color: color, thickness: thickness,
+                         width: width, kind: ebkPercentAsym)
     result.percentPlus  = err.p
     result.percentMinus = err.m
 
-func newErrorBar*[T: SomeNumber](color: Color, thickness = 0.0, width = 0.0, visible = true): ErrorBar[T] =
+func newErrorBar*[T: SomeNumber](color: Color, thickness = 0.0, width = 0.0,
+                                 visible = true): ErrorBar[T] =
   ## creates an `ErrorBar` object of type `ebkSqrt`
-  result = ErrorBar[T](visible: visible, color: color, thickness: thickness, width: width, kind: ebkSqrt)
+  result = ErrorBar[T](visible: visible, color: color, thickness: thickness,
+                         width: width, kind: ebkSqrt)
 
-func newErrorBar*[T](err: seq[T], color: Color, thickness = 0.0, width = 0.0, visible = true): ErrorBar[T] =
+func newErrorBar*[T](err: seq[T], color: Color, thickness = 0.0, width = 0.0,
+                     visible = true): ErrorBar[T] =
   ## creates an `ErrorBar` object of type `ebkArraySym`
-  result = ErrorBar[T](visible: visible, color: color, thickness: thickness, width: width, kind: ebkArraySym)
+  result = ErrorBar[T](visible: visible, color: color, thickness: thickness,
+                       width: width, kind: ebkArraySym)
   result.errors = err
 
-func newErrorBar*[T: SomeNumber](err: tuple[p, m: seq[T]], color: Color, thickness = 0.0, width = 0.0, visible = true): ErrorBar[T] =
-  ## creates an `ErrorBar` object of type `ebkArrayAsym`
-  result = ErrorBar[T](visible: visible, color: color, thickness: thickness, width: width, kind: ebkArraySym)
+func newErrorBar*[T: SomeNumber](err: tuple[p, m: seq[T]], color: Color,
+                                 thickness = 0.0, width = 0.0, visible = true): ErrorBar[T] =
+  ## creates an `ErrorBar` object of type `ebkArrayAsym`, where the first
+  result = ErrorBar[T](visible: visible, color: color, thickness: thickness,
+                       width: width, kind: ebkArrayAsym)
   result.errorsPlus  = err.p
   result.errorsMinus = err.m
 
